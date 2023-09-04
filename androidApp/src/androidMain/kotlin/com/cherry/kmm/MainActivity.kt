497152137d1a20a2ebf1cc665aa4e99a5eb4bb6c
CherryLover <jiangjiwei0322@outlook.com>
2023-09-04 10:02:45 +0800

    feat: 🎸 更新包名及应用名称

--- androidApp/src/androidMain/kotlin/com/cherry/kmm/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/com/cherry/kmm/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.myapplication
+package com.cherry.kmm
 
 import MainView
 import android.os.Bundle
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "MyApplication"
+rootProject.name = "KmmNoDb"
 
 include(":androidApp")
 include(":shared")
